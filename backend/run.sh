da84cbcd696321fa712086f4e78a734b7290a1fc
Alexander <mail@fomalhaut.su>
2021-01-08 17:28:47 +0300

    Add virtualenv in run.sh in Backend

--- backend/run.sh
+++ backend/run.sh
@@ -5,4 +5,5 @@ export FLASK_SECRET_KEY=zahx4IleireiYou4edochee5vooh5Eij5Choqu4aiqueiShuY0
 export FLASK_APP=api.py
 export FLASK_PORT=5000
 
+source ./.venv/bin/activate
 flask run --port $FLASK_PORT
